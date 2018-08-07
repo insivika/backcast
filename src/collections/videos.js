0949c20149bde9fc8d591e513b3d5bced00cd788
Allen Hsieh <allenhsiehhr@gmail.com>
2018-08-06 22:45:25 -0700

    End of night commit, working on youtube ajax call

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -2,4 +2,37 @@ var Videos = Backbone.Collection.extend({
 
   model: Video,
 
+  search: function(keyword) {
+    console.log('key', window.YOUTUBE_API_KEY);
+    // var dataObj = {
+    //   maxResults: '5',
+    //   part: 'snippet',
+    //   q: keyword,
+    //   key: window.YOUTUBE_API_KEY,
+    //   type: ''
+    // };
+    var dataObj = { data: {q: keyword} };
+    this.fetch(dataObj);
+
+  },
+
+  fetch: function(dataObj) {
+    $.ajax({
+      method: 'GET',
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      dataObj,
+      success: function(data) {
+        console.log('fetching');
+
+        console.log(data);
+
+        console.log('The data has been fetched');
+      },
+      error: function(data) {
+        console.log('Unable to fetch data', data);
+      }
+
+    });
+  }
+
 });
