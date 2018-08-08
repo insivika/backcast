f0b696b449678ad34ad3d5dc366659916875824f
Allen Hsieh <allenhsiehhr@gmail.com>
2018-08-07 18:21:58 -0700

    Started the search from scratch with backbone's built in fetch method

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -2,14 +2,31 @@ var Videos = Backbone.Collection.extend({
 
   model: Video,
 
+  url: 'https://www.googleapis.com/youtube/v3/search',
+
+  search: function(keywords) {
+    this.fetch(
+      {data: {
+        q: keywords,
+        maxResults: 5,
+        key: window.YOUTUBE_API_KEY,
+        part: 'snippet',
+        videoEmbeddable: true
+      },
+      }
+    );
+  },
+
+  parse: function(videos) {
+    console.log('We got videos!');
+    return videos.items;
+  }
 
 
   // PHILIPPP!!!!! OMGGG!!! http://backbonejs.org/#API-integration
   // http://backbonejs.org/#Collection-fetch
-
   // http://backbonejs.org/#Collection-parse
   // http://backbonejs.org/#Sync
-
   // https://stackoverflow.com/questions/38796670/backbone-js-setting-header-for-get-request
 
 });
